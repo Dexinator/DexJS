e8b440c9105eee2f155222f188fbf09c29309efe
Dexinator <badillo.delgado.jorge@gmail.com>
2025-03-06 15:31:37 -0600

    working on drawer and experience

--- src/types/experience.props.ts
+++ src/types/experience.props.ts
@@ -1,9 +1,10 @@
 export interface ExperienceProps {
   dates: string;
   title: string;
-  company: string;
-  companyUrl: string;
+  organization: string; // Cambiado de "company" a "organization" para mayor flexibilidad
+  organizationUrl?: string; // Ahora opcional, ya que algunas experiencias no tienen sitio web
   description: string;
   technologies: string[];
   logo?: string;
+  category?: "Coaching y Educación" | "Comunicación y Proyectos"; // Nueva categoría opcional
 }
